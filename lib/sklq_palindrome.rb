52c76b0d1fc046f9d2690590f69ff477027654d2
Samantha Koh <26067359+samkohlq@users.noreply.github.com>
2020-05-29 22:32:04 +0800

    initialise repo

---
+++ lib/sklq_palindrome.rb
@@ -0,0 +1,6 @@
+require "sklq_palindrome/version"
+
+module SklqPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sklq_palindrome/version.rb
@@ -0,0 +1,3 @@
+module SklqPalindrome
+  VERSION = "0.1.0"
+end
